9a1748253df8335f20ec3db6af1e93b0b80a860d
acaiibawl <hagiya0719@gmail.com>
2021-12-23 16:46:52 +0900

    Card一覧をAPIから取得。カラムごとに並び替え

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import produce from 'immer'
-import { randomID } from './util'
+import { randomID, sortBy } from './util'
 import { api } from './api'
 import { Header as _Header } from './Header'
 import { Column } from './Column'
 import { DeleteDialog } from './DeleteDialog'
 import { Overlay as _Overlay } from './Overlay'
 
+type State = {
+  columns?: {
+    id: string
+    title?: string
+    text?: string
+    cards?: {
+      id: string
+      text?: string
+    }[]
+  }[]
+  cardsOrder: Record<string, string>
+}
 export const App = () => {
   const [filterValue, setFilterValue] = useState('')
-  const [columns, setColumns] = useState([
-    {
-      id: 'A',
-      title: 'TODO',
-      text: '',
-      cards: [
-        { id: 'a', text: '朝食をとる🍞' },
-        { id: 'b', text: 'SNSをチェックする🐦' },
-        { id: 'c', text: '布団に入る (:3[___]' },
-      ],
-    },
-    {
-      id: 'B',
-      title: 'Doing',
-      text: '',
-      cards: [
-        { id: 'd', text: '顔を洗う👐' },
-        { id: 'e', text: '歯を磨く🦷' },
-      ],
-    },
-    {
-      id: 'C',
-      title: 'Waiting',
-      text: '',
-      cards: [],
-    },
-    {
-      id: 'D',
-      title: 'Done',
-      text: '',
-      cards: [{ id: 'f', text: '布団から出る (:3っ)っ -:三[＿＿]' }],
-    },
-  ])
+  const [{ columns }, setData] = useState<State>({ cardsOrder: {} })
+
+  useEffect(() => {
+    ;(async () => {
+      const columns = await api('GET /v1/columns', null)
+
+      setData(
+        produce((draft: State) => {
+          draft.columns = columns
+        }),
+      )
+
+      const [unorderedCards, cardsOrder] = await Promise.all([
+        api('GET /v1/cards', null),
+        api('GET /v1/cardsOrder', null),
+      ])
+
+      setData(
+        produce((draft: State) => {
+          draft.cardsOrder = cardsOrder
+          draft.columns?.forEach(column => {
+            column.cards = sortBy(unorderedCards, cardsOrder, column.id)
+          })
+        }),
+      )
+    })()
+  }, [])
 
   const [draggingCardID, setDraggingCardID] = useState<string | undefined>(
     undefined,
@@ -56,25 +62,24 @@ export const App = () => {
 
     if (fromID === toID) return
 
-    type Columns = typeof columns
-    setColumns(
-      produce((columns: Columns) => {
-        const card = columns
-          .flatMap(col => col.cards)
+    setData(
+      produce((draft: State) => {
+        const card = draft.columns
+          ?.flatMap(col => col.cards ?? [])
           .find(c => c.id === fromID)
         if (!card) return
 
-        const fromColumn = columns.find(col =>
-          col.cards.some(c => c.id === fromID),
+        const fromColumn = draft.columns?.find(col =>
+          col.cards?.some(c => c.id === fromID),
         )
-        if (!fromColumn) return
+        if (!fromColumn?.cards) return
 
         fromColumn.cards = fromColumn.cards.filter(c => c.id !== fromID)
 
-        const toColumn = columns.find(
-          col => col.id === toID || col.cards.some(c => c.id === toID),
+        const toColumn = draft.columns?.find(
+          col => col.id === toID || col.cards?.some(c => c.id === toID),
         )
-        if (!toColumn) return
+        if (!toColumn?.cards) return
 
         let index = toColumn.cards.findIndex(c => c.id === toID)
         if (index < 0) {
@@ -86,40 +91,38 @@ export const App = () => {
   }
 
   const setText = (columnID: string, value: string) => {
-    type Columns = typeof columns
-    setColumns(
-      produce((columns: Columns) => {
-        const column = columns.find(c => c.id === columnID)
+    setData(
+      produce((draft: State) => {
+        const column = draft.columns?.find(c => c.id === columnID)
         if (!column) return
 
         column.text = value
-      })
+      }),
     )
   }
 
   const addCard = (columnID: string) => {
-    const column = columns.find(c => c.id === columnID)
+    const column = columns?.find(c => c.id === columnID)
     if (!column) return
     const text = column.text
     const cardID = randomID()
 
-    type Columns = typeof columns
-    setColumns(
-      produce((columns: Columns) => {
-        const column = columns.find(c => c.id === columnID)
-        if(!column) return
+    setData(
+      produce((draft: State) => {
+        const column = draft.columns?.find(c => c.id === columnID)
+        if (!column) return
 
-        column.cards.unshift({
+        column.cards?.unshift({
           id: cardID,
-          text: column.text
+          text: column.text,
         })
         column.text = ''
-      })
+      }),
     )
 
     api('POST /v1/cards', {
       id: cardID,
-      text
+      text,
     })
   }
 
@@ -133,14 +136,15 @@ export const App = () => {
 
     setDeletingCardID(undefined)
 
-    type Columns = typeof columns
-    setColumns(
-      produce((columns: Columns) => {
-        const column = columns.find(col => col.cards.some(c => c.id === cardID))
+    setData(
+      produce((draft: State) => {
+        const column = draft.columns?.find(col =>
+          col.cards?.some(c => c.id === cardID),
+        )
         if (!column) return
 
-        column.cards = column.cards.filter(c => c.id !== cardID)
-      })
+        column.cards = column.cards?.filter(c => c.id !== cardID)
+      }),
     )
   }
 
@@ -150,7 +154,10 @@ export const App = () => {
 
       <MainArea>
         <HorizontalScroll>
-          {columns.map(({ id: columnID, title, cards, text }) => (
+          {!columns ? (
+            <Loading />
+          ) : (
+          columns.map(({ id: columnID, title, cards, text }) => (
             <Column
               key={columnID}
               title={title}
@@ -163,7 +170,8 @@ export const App = () => {
               onTextChange={value => setText(columnID, value)}
               onTextConfirm={() => addCard(columnID)}
             />
-          ))}
+          ))
+          )}
         </HorizontalScroll>
       </MainArea>
 
@@ -211,6 +219,12 @@ const HorizontalScroll = styled.div`
     flex: 0 0 16px;
     content: '';
   }
+`
+
+const Loading = styled.div.attrs({
+  children: 'Loading...',
+})`
+  font-size: 14px;
 `
 
 const Overlay = styled(_Overlay)`
