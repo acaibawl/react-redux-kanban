e223977805ae9c5ab505fbd9a43e18c5dec40f86
acaiibawl <hagiya0719@gmail.com>
2021-12-24 12:36:54 +0900

    Column と card の読み込みを Redux で実装しなおす。utilモジュールをunit test

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,20 +1,55 @@
 import { Reducer } from 'redux'
 import produce from 'immer'
+import { sortBy } from './util'
+import { CardID, ColumnID } from './api'
 
 export type State = {
   filterValue: string
+  columns?: {
+    id: ColumnID
+    title?: string
+    text?: string
+    cards?: {
+      id: CardID
+      test?: string
+    }[]
+  }[]
+  cardsOrder: Record<string, CardID | ColumnID | null>
 }
 
 const initialState: State = {
   filterValue: '',
+  cardsOrder: {},
 }
 
-export type Action = {
-  type: 'Filter.SetFilter'
-  payload: {
-    value: string
-  }
-}
+// 共用体型として定義。パイプで繋いだうちのどれかに該当する型を表す。
+export type Action =
+  | {
+      type: 'Filter.SetFilter'
+      payload: {
+        value: string
+      }
+    }
+  | {
+      type: 'App.SetColumns'
+      payload: {
+        columns: {
+          id: ColumnID
+          title?: string
+          text?: string
+        }[]
+      }
+    }
+  | {
+      type: 'App.SetCards'
+      payload: {
+        cards: {
+          id: CardID
+          text?: string
+        }[]
+        cardsOrder: Record<string, CardID | ColumnID | null>
+      }
+    }
 
 export const reducer: Reducer<State, Action> = produce(
   (draft: State, action: Action) => {
@@ -24,6 +59,28 @@ export const reducer: Reducer<State, Action> = produce(
 
         draft.filterValue = value
         return
+      }
+
+      case 'App.SetColumns': {
+        const { columns } = action.payload
+
+        draft.columns = columns
+        return
+      }
+
+      case 'App.SetCards': {
+        const { cards: unorderedCards, cardsOrder } = action.payload
+
+        draft.cardsOrder = cardsOrder
+        draft.columns?.forEach(column => {
+          column.cards = sortBy(unorderedCards, cardsOrder, column.id)
+        })
+        return
+      }
+
+      // switch文のcaseがすべてのaction.typeを網羅していることを型検査で検出するために必要
+      default: {
+        const _: never = action
       }
     }
   },
--- test/reducer.test.ts
+++ test/reducer.test.ts
@@ -2,6 +2,7 @@ import baretest from 'baretest'
 import assert from 'assert'
 import produce from 'immer'
 import { reducer, State } from '../src/reducer'
+import { ColumnID, CardID } from '../src/api'
 
 const test = baretest('reducer')
 // テスト開始の関数をファイル内の行が最後まで実行された後に実行されるように登録
@@ -9,6 +10,7 @@ setImmediate(() => test.run())
 
 const initialState: State = {
   filterValue: '',
+  cardsOrder: {},
 }
 
 // 引数1=テスト名、引数2=テスト内容
@@ -32,5 +34,75 @@ test('Filter.SetFilter', async () => {
   })
 
   // reducerで実行した後の状態と、Immerで作った期待される状態を比較
+  assert.deepStrictEqual(next, expected)
+})
+
+test('App.SetCards', async () => {
+  const prev = produce(initialState, draft => {
+    draft.columns = [
+      {
+        id: 'A' as ColumnID,
+      },
+      {
+        id: 'B' as ColumnID,
+      },
+    ]
+  })
+
+  const next = reducer(prev, {
+    type: 'App.SetCards',
+    payload: {
+      cards: [
+        {
+          id: '3' as CardID,
+        },
+        {
+          id: '2' as CardID,
+        },
+        {
+          id: '1' as CardID,
+        },
+      ],
+      cardsOrder: {
+        A: '1' as CardID,
+        '1': '2' as CardID,
+        '2': 'A' as CardID,
+        B: '3' as CardID,
+        '3': 'B' as CardID,
+      },
+    },
+  })
+
+  const expected = produce(prev, draft => {
+    draft.cardsOrder = {
+      A: '1' as CardID,
+      '1': '2' as CardID,
+      '2': 'A' as CardID,
+      B: '3' as CardID,
+      '3': 'B' as CardID,
+    }
+    draft.columns = [
+      {
+        id: 'A' as ColumnID,
+        cards: [
+          {
+            id: '1' as CardID,
+          },
+          {
+            id: '2' as CardID,
+          },
+        ],
+      },
+      {
+        id: 'B' as ColumnID,
+        cards: [
+          {
+            id: '3' as CardID,
+          },
+        ],
+      },
+    ]
+  })
+
   assert.deepStrictEqual(next, expected)
 })
